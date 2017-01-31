0d2f2e331f140fac8893bc491627001bb6e9f191
tmukhe001c <trishulpani_mukherjee@comcast.com>
2017-01-31 13:14:20 -0500

    some bootstrap theming added

--- frontend/src/components/HelloServer.vue
+++ frontend/src/components/HelloServer.vue
@@ -1,15 +1,23 @@
 <template>
- <div class="helloServer">
-  <p style="color:red">{{ username }}</p>
-  <p style="color:blue">{{ someStaticData }}</p>
 
-  <alert :show.sync="showTop" placement="top" duration="3000" type="danger" width="400px" dismissable>
-    <span class="icon-info-circled alert-icon-float-left"></span>
-    <strong>Heads up!</strong>
-    <p>This alert needs your attention.</p>
-  </alert>
+ <div class="row">
+             <div class="col-lg-12 text-center">
+                 <div class="helloServer">
+                  <p style="color:red">{{ username }}</p>
+                  <p style="color:blue">{{ someStaticData }}</p>
 
- </div>
+                  <!--
+                  <alert :show.sync="showTop" placement="top" duration="3000" type="danger" width="400px" dismissable>
+                    <span class="icon-info-circled alert-icon-float-left"></span>
+                    <strong>Heads up!</strong>
+                    <p>This alert needs your attention.</p>
+                  </alert>
+                  -->
+                 </div>
+             </div>
+  </div>
+
+
 </template>
 
 <script>

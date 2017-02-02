191174e3c9b95a6447af88458149ec84e3810bb2
tmukhe001c <trishulpani_mukherjee@comcast.com>
2017-02-02 16:46:21 -0500

    Added component

---
+++ frontend/src/components/ForumTopics.vue
@@ -0,0 +1,28 @@
+<template>
+ <div class="row-fluid">
+   <div class="forumTopics">
+      <div class="span9">
+        <div id="listStuff" style="height:90%;">
+          <div class="row-fluid">
+            <div class="span12">
+              <a href="/topics/1" title="Forum posts template with bootstrap 3.2">Sample post 1 {{counter}}</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+   </div>
+</template>
+
+<script>
+
+export default {
+  name: 'forumTopics',
+  data: function () {
+      return {
+         counter: 0
+      }
+   }
+}
+
+</script>
